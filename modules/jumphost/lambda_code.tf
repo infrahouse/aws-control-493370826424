dfac2433d3f4b4b6c5cd886a2e667fcbf2868eb1
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2023-07-13 16:47:40 -0700

    Upload lamda code to S3 (#5)
    * Upload lamda code to S3
    
    In order to apply the plan Terraform needs update_dns.zip that was
    created at the plan stage. The file is not available in the subsequent
    apply run. This PR uploads and downloads the lambda code archive.
    
    * Emulate code change
    
    * Tune lamda packaging
    
    * Store lambda code in S3
    
    * No need to upload/remove lambda code manually

--- modules/jumphost/lambda_code.tf
+++ modules/jumphost/lambda_code.tf
@@ -1,31 +1,57 @@
+locals {
+  lambda_root = "${path.module}/update_dns"
+}
 resource "null_resource" "install_python_dependencies" {
   provisioner "local-exec" {
     command = "bash ${path.module}/package_update_dns.sh"
     environment = {
-      TARGET_DIR        = "${path.module}/update_dns"
-      REQUIREMENTS_FILE = "${path.module}/update_dns/requirements.txt"
+      TARGET_DIR        = local.lambda_root
+      MODULE_DIR        = path.module
+      REQUIREMENTS_FILE = "${local.lambda_root}/requirements.txt"
     }
   }
-  depends_on = [
-    null_resource.clean_up
-  ]
   triggers = {
-    dependencies = filemd5("${path.module}/update_dns/requirements.txt")
-    code         = filemd5("${path.module}/update_dns/main.py")
+    dependencies_version = filemd5("${local.lambda_root}/requirements.txt")
+    main_version         = filemd5("${local.lambda_root}/main.py")
   }
 }
 
-resource "null_resource" "clean_up" {
-  provisioner "local-exec" {
-    command = "rm -f ${path.module}/update_dns.zip"
+resource "random_uuid" "lamda_src_hash" {
+  keepers = {
+    for filename in setunion(
+      fileset(local.lambda_root, "main.py"),
+      fileset(local.lambda_root, "requirements.txt")
+    ) :
+    filename => filemd5("${local.lambda_root}/${filename}")
   }
 }
 
 data "archive_file" "lambda" {
-  type        = "zip"
+  type = "zip"
+  excludes = [
+    "__pycache__"
+  ]
   source_dir  = "${path.module}/update_dns"
-  output_path = "${path.module}/update_dns.zip"
+  output_path = "${path.module}/${random_uuid.lamda_src_hash.result}.zip"
   depends_on = [
     null_resource.install_python_dependencies
   ]
+}
+
+resource "aws_s3_bucket" "lambda_tmp" {
+  bucket_prefix = "infrahouse-jumphost-lambda-"
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.lambda_tmp.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_object" "lambda_package" {
+  bucket = aws_s3_bucket.lambda_tmp.bucket
+  key    = basename(data.archive_file.lambda.output_path)
+  source = data.archive_file.lambda.output_path
 }
