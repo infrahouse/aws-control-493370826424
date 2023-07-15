83175dbefb002af817ef3fe44e2cef41ad878e98
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2023-07-15 16:30:19 -0700

    Jumphost Lambda updates DNS (#14)
    * Jumphost Lambda updates DNS
    
    * Adds an IP to the A record if the new instance is launching
    * Removes the IP from the A record if the new instance is terminating
    
    * fix env variable format

--- modules/jumphost/update_dns/main.py
+++ modules/jumphost/update_dns/main.py
@@ -1,3 +1,4 @@
+from os import environ
 from pprint import pprint
 import boto3
 
@@ -19,14 +20,113 @@ def complete_lifecycle_action(
     )
 
 
-def add_record():
+def add_record(zone_id, hostname, instance_id, ttl, region):
     """Add the instance to DNS."""
-    pass
+    zone_name = get_zone_name(zone_id)
+    public_ip = get_public_ip(instance_id, region)
+
+    route53_client = boto3.client("route53")
+    response = route53_client.list_resource_record_sets(
+        HostedZoneId=zone_id,
+        StartRecordType="A",
+        StartRecordName=f"{hostname}.{zone_name}",
+        MaxItems="1",
+    )
+    ip_set = {public_ip}
+    for rr_set in response["ResourceRecordSets"]:
+        for rr in rr_set["ResourceRecords"]:
+            ip_set.add(rr["Value"])
+
+    r_records = [{"Value": ip} for ip in list(ip_set)]
+    route53_client.change_resource_record_sets(
+        HostedZoneId=zone_id,
+        ChangeBatch={
+            "Changes": [
+                {
+                    "Action": "UPSERT",
+                    "ResourceRecordSet": {
+                        "Name": f"{hostname}.{zone_name}",
+                        "Type": "A",
+                        "ResourceRecords": r_records,
+                        "TTL": ttl,
+                    },
+                }
+            ]
+        },
+    )
 
 
-def remove_record():
+def remove_record(zone_id, hostname, instance_id, ttl, region):
     """Remove the instance from DNS."""
-    pass
+    zone_name = get_zone_name(zone_id)
+    public_ip = get_public_ip(instance_id, region)
+
+    route53_client = boto3.client("route53")
+    response = route53_client.list_resource_record_sets(
+        HostedZoneId=zone_id,
+        StartRecordType="A",
+        StartRecordName=f"{hostname}.{zone_name}",
+        MaxItems="1",
+    )
+    ip_set = set()
+    for rr_set in response["ResourceRecordSets"]:
+        for rr in rr_set["ResourceRecords"]:
+            ip = rr["Value"]
+            if ip != public_ip:
+                ip_set.add(rr["Value"])
+    r_records = [{"Value": ip} for ip in list(ip_set)]
+    if r_records:
+        route53_client.change_resource_record_sets(
+            HostedZoneId=zone_id,
+            ChangeBatch={
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": f"{hostname}.{zone_name}",
+                            "Type": "A",
+                            "ResourceRecords": r_records,
+                            "TTL": ttl,
+                        },
+                    }
+                ]
+            },
+        )
+    else:
+        route53_client.change_resource_record_sets(
+            HostedZoneId=zone_id,
+            ChangeBatch={
+                "Changes": [
+                    {
+                        "Action": "DELETE",
+                        "ResourceRecordSet": {
+                            "Name": f"{hostname}.{zone_name}",
+                            "Type": "A",
+                            "ResourceRecords": [{"Value": public_ip}],
+                            "TTL": ttl,
+                        },
+                    }
+                ]
+            },
+        )
+
+
+def get_public_ip(instance_id, region):
+    """Get the instance's public IP address by its instance_id"""
+    ec2_client = boto3.client("ec2", region=region)
+    return ec2_client.describe_instances(
+        InstanceIds=[
+            instance_id,
+        ],
+    )["Reservations"][
+        0
+    ]["Instances"][0]["PublicIpAddress"]
+
+
+def get_zone_name(zone_id):
+    """Get zone name, i.e. infrahouse.com by zone_id"""
+    route53_client = boto3.client("route53")
+    return route53_client.get_hosted_zone(Id=zone_id)["HostedZone"]["Name"]
 
 
 def lambda_handler(event, context):
@@ -39,9 +139,21 @@ def lambda_handler(event, context):
     pprint(response)
 
     if event["detail"]["LifecycleTransition"] == "autoscaling:EC2_INSTANCE_TERMINATING":
-        remove_record()
+        remove_record(
+            environ["ROUTE53_ZONE_ID"],
+            environ["ROUTE53_HOSTNAME"],
+            event["EC2InstanceId"],
+            environ["ROUTE53_TTL"],
+            environ["AWS_REGION"],
+        )
     elif event["detail"]["LifecycleTransition"] == "autoscaling:EC2_INSTANCE_LAUNCHING":
-        add_record()
+        add_record(
+            environ["ROUTE53_ZONE_ID"],
+            environ["ROUTE53_HOSTNAME"],
+            event["EC2InstanceId"],
+            environ["ROUTE53_TTL"],
+            environ["AWS_REGION"],
+        )
 
     complete_lifecycle_action(
         lifecyclehookname=event["detail"]["LifecycleHookName"],
