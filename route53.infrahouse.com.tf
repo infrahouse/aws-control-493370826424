31699d9d10b1af27a9b301de1874cf49d17c6e85
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2023-07-15 11:30:26 -0700

    Add MX and other mail related records (#9)

--- route53.infrahouse.com.tf
+++ route53.infrahouse.com.tf
@@ -2,3 +2,48 @@ resource "aws_route53_zone" "infrahouse_com" {
   provider = aws.aws-493370826424-uw1
   name     = "infrahouse.com"
 }
+
+## MX
+resource "aws_route53_record" "mx" {
+  name    = aws_route53_zone.infrahouse_com.name
+  type    = "MX"
+  zone_id = aws_route53_zone.infrahouse_com.id
+  ttl     = 3600
+  records = [
+    "1 aspmx.l.google.com.",
+    "5 alt1.aspmx.l.google.com.",
+    "5 alt2.aspmx.l.google.com.",
+    "10 alt3.aspmx.l.google.com.",
+    "10 alt4.aspmx.l.google.com."
+  ]
+}
+
+resource "aws_route53_record" "spf" {
+  name    = aws_route53_zone.infrahouse_com.name
+  type    = "SPF"
+  zone_id = aws_route53_zone.infrahouse_com.id
+  records = [
+    "\"v=spf1 include:_spf.google.com ~all\""
+  ]
+  ttl = 3600
+}
+
+resource "aws_route53_record" "txt_spf" {
+  name    = aws_route53_zone.infrahouse_com.name
+  type    = "TXT"
+  zone_id = aws_route53_zone.infrahouse_com.id
+  records = [
+    "\"v=spf1 include:_spf.google.com ~all\""
+  ]
+  ttl = 3600
+}
+
+resource "aws_route53_record" "txt_dkim" {
+  name    = "google._domainkey.${aws_route53_zone.infrahouse_com.name}"
+  type    = "TXT"
+  zone_id = aws_route53_zone.infrahouse_com.id
+  records = [
+    "\"v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhRsHTvHGTZIg60W5xybWjEAa3dbxh1qUxM5yS4LAEOtPm6dlNO88SMbXQP+h82kffiOukmWnEgzoUmDs8yEqn9AuMpEsVLH5tLbzpQhWOAzEJ5aLQKtgb5snES2gdkb2jeZX60dhBm9CzlZHhBiSZRp1+hWu02f9aLe2WYwFsL/lsQsTwzYN8mTc83l/UQ2e/\" \"adsylAdWJlPmd9p8VIr63aMLVBmid2AWDQnj1RgKOHNARz6d3meTg602EEiI0+0OzHd8Zdc16kY3NUYZ1u8GBuCA5hleAYtefshhmwn5pSg375urhl3+480vp0H/jyq+DGYrDZ5Cqwgd2HjSCrzFwIDAQAB\""
+  ]
+  ttl = 3600
+}
