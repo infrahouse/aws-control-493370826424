f2a6566598abb6e4140a4feb983f62e2c109bccf
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-07-15 19:46:27 -0700

    Change lamda code to test upload

--- modules/jumphost/update_dns/main.py
+++ modules/jumphost/update_dns/main.py
@@ -21,6 +21,7 @@ def complete_lifecycle_action(
 
 def add_record(zone_id, hostname, instance_id, ttl, region):
     """Add the instance to DNS."""
+    print(f"Adding instance {instance_id} as a hostname {hostname} to zone {zone_id}")
     zone_name = get_zone_name(zone_id)
     public_ip = get_public_ip(instance_id, region)
 
@@ -57,6 +58,7 @@ def add_record(zone_id, hostname, instance_id, ttl, region):
 
 def remove_record(zone_id, hostname, instance_id, ttl, region):
     """Remove the instance from DNS."""
+    print(f"Removing instance {instance_id} from zone {zone_id}")
     zone_name = get_zone_name(zone_id)
     public_ip = get_public_ip(instance_id, region)
 
