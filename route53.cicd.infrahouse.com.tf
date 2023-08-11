848a2b79f5db1b2023028d0aef2dfd7bb0749433
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-11 11:12:53 -0700

    Make resource name accurate

--- route53.cicd.infrahouse.com.tf
+++ route53.cicd.infrahouse.com.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "mx" {
+resource "aws_route53_record" "ci-cd-ns" {
   provider = aws.aws-493370826424-uw1
   name     = data.aws_route53_zone.cicd-ih-com.name
   type     = "NS"
