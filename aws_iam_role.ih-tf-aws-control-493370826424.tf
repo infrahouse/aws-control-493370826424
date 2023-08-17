4870cd1f168b7f7e66657cf8176a6b57ff09345e
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-16 18:05:16 -0700

    Use public infrahouse/gha-admin/aws module

--- aws_iam_role.ih-tf-aws-control-493370826424.tf
+++ aws_iam_role.ih-tf-aws-control-493370826424.tf
@@ -1,11 +1,16 @@
 ## Roles for CI/CD in the aws-control-493370826424 repo
 
 module "ih-tf-aws-control-493370826424-admin" {
-  source = "github.com/infrahouse/terraform-aws-gha-admin"
+  source  = "infrahouse/gha-admin/aws"
+  version = "~> 1.0"
   providers = {
     aws = aws.aws-493370826424-uw1
   }
   gh_identity_provider_arn = module.github-connector.gh_openid_connect_provider_arn
+  gh_org_name              = "infrahouse"
   repo_name                = "aws-control-493370826424"
   state_bucket             = "infrahouse-aws-control-493370826424"
+  admin_allowed_arns = [
+    "arn:aws:iam::990466748045:user/aleks"
+  ]
 }
