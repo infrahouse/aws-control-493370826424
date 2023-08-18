520f99aaae1ea3ee625d9691a5e67ca1f164a6a6
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-18 07:54:16 -0700

    Expose roles for website management

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,13 @@ output "admin_role_arn" {
   description = "ARN of a role that can manage resources via terraform."
   value       = module.ih-tf-aws-control-493370826424-admin.admin_role_arn
 }
+
+output "infrahouse-website-infra-admin-role" {
+  description = "website admin role ARN"
+  value       = module.ih-tf-infrahouse-website-infra-admin.admin_role_arn
+}
+
+output "infrahouse-website-infra-github-role" {
+  description = "website GitHub Actions role ARN"
+  value       = module.ih-tf-infrahouse-website-infra-admin.github_role_arn
+}
