67a05f8c41fbdb0606e0e3b06f3da041314326e9
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2023-08-17 20:37:58 -0700

    Add GHA roles for repo infrahouse-website-infra

---
+++ aws_iam_role.ih-tf-infrahouse-website-infra.tf
@@ -0,0 +1,16 @@
+## Roles for CI/CD in the aws-control-493370826424 repo
+
+module "ih-tf-infrahouse-website-infra-admin" {
+  source  = "infrahouse/gha-admin/aws"
+  version = "~> 1.0"
+  providers = {
+    aws = aws.aws-493370826424-uw1
+  }
+  gh_identity_provider_arn = module.github-connector.gh_openid_connect_provider_arn
+  gh_org_name              = "infrahouse"
+  repo_name                = "infrahouse-website-infra"
+  state_bucket             = "infrahouse-infrahouse-website-infra"
+  admin_allowed_arns = [
+    "arn:aws:iam::990466748045:user/aleks"
+  ]
+}
