aa72ffd3889d7638a1a637d7d70ce5ca98f29697
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-03-02 11:44:58 -0800

    Deploy tapir for terraform registry

---
+++ registry.tf
@@ -0,0 +1,17 @@
+module "ecs" {
+  source = "git::https://github.com/infrahouse/terraform-aws-ecs.git?ref=2.6.1"
+  providers = {
+    aws     = aws
+    aws.dns = aws
+  }
+  asg_subnets = module.management.subnet_private_ids
+  dns_names = [
+    "registry"
+  ]
+  docker_image          = "pacovk/tapir"
+  internet_gateway_id   = module.management.internet_gateway_id
+  load_balancer_subnets = module.management.subnet_public_ids
+  service_name          = "terraform-registry"
+  ssh_key_name          = aws_key_pair.aleks.key_name
+  zone_id               = module.infrahouse_com.infrahouse_zone_id
+}
