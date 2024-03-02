ce2d008886662c2c3e172823b06a712c299fa6c4
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-03-02 12:10:00 -0800

    [registry]: set healthcheck command

--- registry.tf
+++ registry.tf
@@ -8,10 +8,11 @@ module "ecs" {
   dns_names = [
     "registry"
   ]
-  docker_image          = "pacovk/tapir"
-  internet_gateway_id   = module.management.internet_gateway_id
-  load_balancer_subnets = module.management.subnet_public_ids
-  service_name          = "terraform-registry"
-  ssh_key_name          = aws_key_pair.aleks-Black-MBP.key_name
-  zone_id               = module.infrahouse_com.infrahouse_zone_id
+  docker_image                  = "pacovk/tapir"
+  internet_gateway_id           = module.management.internet_gateway_id
+  load_balancer_subnets         = module.management.subnet_public_ids
+  service_name                  = "terraform-registry"
+  ssh_key_name                  = aws_key_pair.aleks-Black-MBP.key_name
+  zone_id                       = module.infrahouse_com.infrahouse_zone_id
+  container_healthcheck_command = "curl -f http://localhost:8080/ || exit 1"
 }
