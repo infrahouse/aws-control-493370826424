dbea9eb31744065af135836d6906027f157c2253
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2024-03-03 08:53:24 -0800

    Configure default AWS provider

--- providers.tf
+++ providers.tf
@@ -1,3 +1,17 @@
+# Default provider
+provider "aws" {
+  region = "us-west-1"
+  assume_role {
+    role_arn = "arn:aws:iam::493370826424:role/ih-tf-aws-control-493370826424-admin"
+  }
+  default_tags {
+    tags = {
+      "created_by" : "infrahouse/aws-control-493370826424" # GitHub repository that created a resource
+      "environment" : var.environment
+    }
+  }
+}
+
 provider "aws" {
   alias  = "aws-493370826424-uw1"
   region = "us-west-1"
