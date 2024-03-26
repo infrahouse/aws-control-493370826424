07b39fe7ba3f29db3229c66c59e0221f39603990
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-03-26 14:50:36 -0700

    Add MX for twindb.com

--- twindb-zone.tf
+++ twindb-zone.tf
@@ -1,3 +1,13 @@
 resource "aws_route53_zone" "twindb_com" {
   name = "twindb.com"
 }
+
+resource "aws_route53_record" "mx_twindb_com" {
+  name    = "twindb.com"
+  type    = "MX"
+  ttl     = 300
+  zone_id = aws_route53_zone.twindb_com.zone_id
+  records = [
+    "10 mail.twindb.com"
+  ]
+}
