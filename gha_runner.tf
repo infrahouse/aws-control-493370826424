15af251ebd85fca666f2e8c94abed141b0060f00
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-03-30 13:57:33 -0700

    Create secret for GitHub token (#166)
    The token will be used in jumphost, to register GitHub self hosted
    runners.

--- gha_runner.tf
+++ gha_runner.tf
@@ -1,24 +1,5 @@
-resource "aws_secretsmanager_secret" "runner_token_secret" {
-  provider                = aws.aws-493370826424-uw1
-  name                    = "runner_token_secret"
-  description             = "Token to register a GitHub Actions runner."
+resource "aws_secretsmanager_secret" "github_token" {
+  name                    = "GITHUB_TOKEN"
+  description             = "GitHub token with manage_runners:org permissions. Needed to register self-hosted runners."
   recovery_window_in_days = 0
-}
-
-data "aws_iam_policy_document" "github_runner_permissions" {
-  statement {
-    actions = [
-      "secretsmanager:*",
-    ]
-    resources = [
-      aws_secretsmanager_secret.runner_token_secret.arn
-    ]
-  }
-}
-
-resource "aws_iam_policy" "github_runner_permissions" {
-  provider    = aws.aws-493370826424-uw1
-  name        = "github_runner_permissions"
-  description = "Policy that allows to manage github runner token"
-  policy      = data.aws_iam_policy_document.github_runner_permissions.json
 }
