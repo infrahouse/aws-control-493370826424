43cc26ea97f7df7927e4d1d42087b71a287a5b6d
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2024-03-30 14:01:16 -0700

    Pass GitHub token to jumphost
    The jumphost will perform a role of github_runner. For that it needs
    GITHUB_TOKEN with managerunner access. Pass it and make it available for
    root only.

--- gha_runner.tf
+++ gha_runner.tf
@@ -3,3 +3,7 @@ resource "aws_secretsmanager_secret" "github_token" {
   description             = "GitHub token with manage_runners:org permissions. Needed to register self-hosted runners."
   recovery_window_in_days = 0
 }
+
+data "aws_secretsmanager_secret_version" "github_token" {
+  secret_id = aws_secretsmanager_secret.github_token.id
+}
