55bd8e08af706df91c6b3877aedceb41daeb0f78
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-07-02 11:01:20 -0700

    Fix permissions for ih-tf-terraform-aws-openvpn-github (#192)
    * Fix permissions for ih-tf-terraform-aws-openvpn-github
    
    * fix role resource address

---
+++ aws_iam_role.ih-tf-terraform-aws-openvpn-github.tf
@@ -0,0 +1,31 @@
+## Roles for CI/CD in the terraform-aws-openvpn repo
+
+data "aws_iam_policy_document" "openvpn-github-permissions" {
+
+  statement {
+    actions = [
+      "ecr-public:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+  statement {
+    actions = ["ecr:*"]
+    resources = [
+      aws_ecrpublic_repository.openvpn-portal.arn
+    ]
+  }
+
+}
+
+resource "aws_iam_policy" "openvpn-github-permissions" {
+  provider    = aws.aws-493370826424-uw1
+  name        = "openvpn-github-permissions"
+  description = "Permissions policy for ih-tf-terraform-aws-openvpn-github role"
+  policy      = data.aws_iam_policy_document.openvpn-github-permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "openvpn-github" {
+  provider   = aws.aws-493370826424-uw1
+  policy_arn = aws_iam_policy.openvpn-github-permissions.arn
+  role       = module.registry-client-roles["terraform-aws-openvpn"].github_role_name
+}
