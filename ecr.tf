87d7b8e05ffe51f344dc41f585a6eb26370cca20
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-07-02 10:23:48 -0700

    Create OpenVPN Portal docker images repository (#191)

--- ecr.tf
+++ ecr.tf
@@ -11,3 +11,15 @@ resource "aws_ecrpublic_repository" "infrahouse" {
   }
 }
 
+resource "aws_ecrpublic_repository" "openvpn-portal" {
+  provider        = aws.aws-493370826424-ue1
+  repository_name = "openvpn-portal"
+  catalog_data {
+    about_text        = "InfraHouse OpenVPN Portal"
+    architectures     = ["x86-64"]
+    description       = "InfraHouse OpenVPN Portal is a web based service that authenticates users and generates OpenVPN profiles."
+    logo_image_blob   = filebase64("${path.module}/files/infrahouse-logo.png")
+    operating_systems = ["Linux"]
+    usage_text        = "For usage details, check out https://registry.terraform.io/modules/infrahouse/openvpn/aws/latest"
+  }
+}
