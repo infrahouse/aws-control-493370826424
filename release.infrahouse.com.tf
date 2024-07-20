2a6133d85c1d602ed914f5fb9d89ce7fd34d7567
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-07-20 13:43:36 -0700

    Allow jumphost publish packages (#206)
    Co-authored-by: Oleksandr Kuzminskyi <aleks@infrahouse.com>

--- release.infrahouse.com.tf
+++ release.infrahouse.com.tf
@@ -32,5 +32,6 @@ module "release_infrahouse_com" {
     module.infrahouse-toolkit-github.github_role_arn,
     module.infrahouse-puppet-data-github.github_role_arn,
     aws_iam_role.infrahouse-com-github.arn,
+    module.jumphost.jumphost_role_arn,
   ]
 }
