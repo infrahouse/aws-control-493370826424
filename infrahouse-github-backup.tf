2e6383363587f7e424d8992e63f746b65e9a1131
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2024-11-14 08:54:31 -0800

    Install infrahouse-puppet-data in github-backup to add infrahouse specific settings (#241)

--- infrahouse-github-backup.tf
+++ infrahouse-github-backup.tf
@@ -13,10 +13,14 @@ module "infrahouse-github-backup-app-key" {
 }
 
 module "terraform-aws-github-backup" {
-  source         = "registry.infrahouse.com/infrahouse/github-backup/aws"
-  version        = "~> 0.2"
-  app_key_secret = module.infrahouse-github-backup-app-key.secret_name
-  key_pair_name  = aws_key_pair.aleks.key_name
-  subnets        = module.management.subnet_private_ids
-  instance_type  = "t3a.small"
+  source                   = "registry.infrahouse.com/infrahouse/github-backup/aws"
+  version                  = "~> 0.2"
+  app_key_secret           = module.infrahouse-github-backup-app-key.secret_name
+  key_pair_name            = aws_key_pair.aleks.key_name
+  subnets                  = module.management.subnet_private_ids
+  instance_type            = "t3a.small"
+  puppet_hiera_config_path = "/opt/infrahouse-puppet-data/environments/${var.environment}/hiera.yaml"
+  packages = [
+    "infrahouse-puppet-data"
+  ]
 }
