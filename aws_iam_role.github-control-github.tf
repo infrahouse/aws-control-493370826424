09547f040b354b747dbf9e3ef5686629c31fbfd2
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-05-14 18:03:13 -0700

    Allow github-control create resource here (#282)
    * Allow github-control create resource here
    
    * fix resource name

---
+++ aws_iam_role.github-control-github.tf
@@ -0,0 +1,15 @@
+module "github-control-github" {
+  providers = {
+    aws = aws.aws-493370826424-uw1
+  }
+  source      = "infrahouse/github-role/aws"
+  version     = "~> 1.0"
+  gh_org_name = "infrahouse8"
+  repo_name   = "github-control"
+}
+
+resource "aws_iam_role_policy_attachment" "github-control-github" {
+  provider   = aws.aws-493370826424-uw1
+  policy_arn = data.aws_iam_policy.administrator-access.arn
+  role       = module.github-control-github.github_role_name
+}
