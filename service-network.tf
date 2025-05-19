d246e25b13319c79f45137266d8f57aadbcb64e3
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-05-19 06:41:59 -0700

    Remove redundant NAT gateway

--- service-network.tf
+++ service-network.tf
@@ -1,6 +1,6 @@
 module "management" {
   source  = "infrahouse/service-network/aws"
-  version = "~> 3.0"
+  version = "3.1.1"
   providers = {
     aws = aws.aws-493370826424-uw1
   }
@@ -8,36 +8,27 @@ module "management" {
   service_name          = "management"
   vpc_cidr_block        = "10.0.0.0/22"
   environment           = var.environment
-  enable_dns_hostnames  = true
-  enable_dns_support    = true
   subnets = [
     {
       cidr                    = "10.0.0.0/24"
       availability-zone       = data.aws_availability_zones.uw1.names[0]
       map_public_ip_on_launch = true
       create_nat              = true
-      forward_to              = ""
     },
     {
-      cidr                    = "10.0.1.0/24"
-      availability-zone       = data.aws_availability_zones.uw1.names[0]
-      map_public_ip_on_launch = false
-      create_nat              = false
-      forward_to              = "10.0.0.0/24"
+      cidr              = "10.0.1.0/24"
+      availability-zone = data.aws_availability_zones.uw1.names[0]
+      forward_to        = "10.0.0.0/24"
     },
     {
       cidr                    = "10.0.2.0/24"
       availability-zone       = data.aws_availability_zones.uw1.names[1]
       map_public_ip_on_launch = true
-      create_nat              = true
-      forward_to              = ""
     },
     {
-      cidr                    = "10.0.3.0/24"
-      availability-zone       = data.aws_availability_zones.uw1.names[1]
-      map_public_ip_on_launch = false
-      create_nat              = false
-      forward_to              = "10.0.2.0/24"
+      cidr              = "10.0.3.0/24"
+      availability-zone = data.aws_availability_zones.uw1.names[1]
+      forward_to        = "10.0.0.0/24"
     }
   ]
 }
