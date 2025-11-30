198e705450323560f0f8024444b60a78b05c86eb
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2025-11-29 18:29:27 -0800

    Deprecate twindb MX

--- twindb-zone.tf
+++ twindb-zone.tf
@@ -2,12 +2,12 @@ resource "aws_route53_zone" "twindb_com" {
   name = "twindb.com"
 }
 
-resource "aws_route53_record" "mx_twindb_com" {
-  name    = "twindb.com"
-  type    = "MX"
-  ttl     = 3600
-  zone_id = aws_route53_zone.twindb_com.zone_id
-  records = [
-    "10 mail.twindb.com"
-  ]
-}
+# resource "aws_route53_record" "mx_twindb_com" {
+#   name    = "twindb.com"
+#   type    = "MX"
+#   ttl     = 3600
+#   zone_id = aws_route53_zone.twindb_com.zone_id
+#   records = [
+#     "10 mail.twindb.com"
+#   ]
+# }
