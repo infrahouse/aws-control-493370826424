20d323c7cd3e2cf51901c8555fb7ca1d1ccc2271
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-11-29 18:43:38 -0800

    Deprecate twindb MX (#352)
    * Deprecate twindb MX
    
    * Remove remaining twindb supporting resources
    
    * Bring back smtp_credentials - used by terraform-aws-github-backup

--- twindb-zone.tf
+++ twindb-zone.tf
@@ -1,13 +1,3 @@
 resource "aws_route53_zone" "twindb_com" {
   name = "twindb.com"
 }
-
-resource "aws_route53_record" "mx_twindb_com" {
-  name    = "twindb.com"
-  type    = "MX"
-  ttl     = 3600
-  zone_id = aws_route53_zone.twindb_com.zone_id
-  records = [
-    "10 mail.twindb.com"
-  ]
-}
