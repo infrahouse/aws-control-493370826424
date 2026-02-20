e07d182e6f58c7cee7ddecd67371a21970869aaf
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2026-02-20 14:22:06 -0800

    Add github-backup module to deploy GitHub backup service (#359)
    Co-authored-by: infrahouse8 <121983874+infrahouse8@users.noreply.github.com>

--- infrahouse-github-backup.tf
+++ infrahouse-github-backup.tf
@@ -11,3 +11,24 @@ module "infrahouse-github-backup-app-key" {
     data.aws_iam_role.AWSAdministratorAccess.arn
   ]
 }
+
+module "github-backup" {
+  source  = "registry.infrahouse.com/infrahouse/github-backup/aws"
+  version = "2.0.1"
+  providers = {
+    aws = aws.aws-493370826424-uw1
+  }
+
+  github_app_id              = "1016509"
+  github_app_installation_id = "55611573"
+
+  alarm_emails   = ["aleks@infrahouse.com"]
+  replica_region = "us-east-1"
+  subnets        = module.management.subnet_private_ids
+
+  github_app_key_secret_writers = [
+    data.aws_iam_role.AWSAdministratorAccess.arn
+  ]
+
+  environment = var.environment
+}
